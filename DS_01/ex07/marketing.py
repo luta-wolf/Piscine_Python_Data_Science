c3ec774e20d3276b6d5b6a3f7bac4b7090b54b5c
Ethelyn Interdictor <einterdi@student.21-school.ru>
2022-09-18 19:58:51 +0300

    day01 finish ex07

---
+++ DS_01/ex07/marketing.py
@@ -0,0 +1,39 @@
+import sys
+
+def dataset():
+	clients = [
+		'andrew@gmail.com',	'jessica@gmail.com',
+		'ted@mosby.com',	'john@snow.is',
+		'bill_gates@live.com',	'mark@facebook.com',
+		'elon@paypal.com',	'jessica@gmail.com'
+		]
+	participants = [
+		'walter@heisenberg.com',	'vasily@mail.ru',
+		'pinkman@yo.org',	'jessica@gmail.com',
+		'elon@paypal.com',	'pinkman@yo.org',
+		'mr@robot.gov',	'eleven@yahoo.com'
+		]
+	recipients = [
+		'andrew@gmail.com',	'jessica@gmail.com',
+		'john@snow.is'
+		]
+	return set(clients), set(participants), set(recipients)
+
+def marketing(argv):
+	clients, participants, recipients = dataset()
+	if argv == "call_center":
+		return  list(clients - recipients)
+	elif argv == "potential_clients":
+		return list(participants  - clients)
+	elif argv == "loyalty_program":
+		return list(clients - participants)
+	else:
+		return "Wrong argument."
+
+
+def main():
+	if len(sys.argv) == 2:
+		print(marketing(sys.argv[1]))
+
+if __name__ == "__main__":
+	main()
