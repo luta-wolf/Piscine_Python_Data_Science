5a8d1b01b281fcde1642677a549731aa0126bafc
Ethelyn Interdictor <einterdi@student.21-school.ru>
2022-09-19 21:49:46 +0300

    day02 finish ex02

--- DS_02/README.md
+++ DS_02/README.md
@@ -2,3 +2,4 @@
 Резюме: сегодня мы поможем вам получить базовые знания об ООП-подходе в Python.
 
 ### ex00
+- [отправить](https://stackoverflow.com/questions/29003305/sending-telegram-message-from-python) сообщение в телеграм бота
--- DS_02/ex02/first_constructor.py
+++ DS_02/ex02/first_constructor.py
@@ -2,16 +2,34 @@ import sys
 import os
 
 class Research:
-	def __init__(self):
-		self.infile = open("data.csv", "r")
-		return self.infile
-		
-	def file_reader(self):
-		infile = __init__()
-		return infile.read()
+	def __init__(self, filename):
+		self.filename = filename
 
-def main():
-	pass
+	def file_reader(self):
+		line = self.check_file()
+		return line
+
+	def check_file(self):
+		if not os.access(self.filename, os.R_OK):
+			raise ValueError("File can't be read")
+		with open(self.filename, 'r') as infile:
+			lines = [line.rstrip() for line in infile]
+		if len(lines) < 2:
+			raise ValueError("Few lines in file")
+		if len(lines[0].split(',')) != 2:
+			raise ValueError("Header is incorrect")
+		for line in lines[1:]:
+			if line != "0,1" and line != "1,0":
+				raise ValueError("Data in the file is incorrect")
+		return "\n".join(lines)
+
 
 if __name__ == "__main__":
-	main()
+	if len(sys.argv) == 2:
+		research = Research(sys.argv[1])
+		print(research.file_reader())
+	else:
+		raise ValueError("Wronn number agruments")
+
+
+
